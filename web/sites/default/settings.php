377a7f86e62da7d6ba750dd2b01d1afa5fa58c22
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2023-03-20 21:09:40 -0400

    include more npr updates.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -762,6 +762,7 @@ $config['npr_api.settings']['npr_api_cds_api_key'] = getenv('GPB_NPR_CDS_API_KEY
 $config['npr_api.settings']['npr_api_url'] = getenv('GPB_NPR_API_URL');
 $config['npr_api.settings']['npr_api_stage_url'] = 'https://api-s1.npr.org';
 $config['npr_pull.settings']['npr_pull_url'] = getenv('GPB_NPR_API_URL');
+$config['npr_pull.settings']['npr_pull_service'] = 'cds';
 
 // Automatically generated include for settings managed by ddev.
 if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
